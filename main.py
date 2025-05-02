37cacc2bce36a7bf2c4ff79601ebc4e04c910330
ellaboring <sche24271@wainuiomatahigh.school.nz>
2025-05-02 14:14:15 +1200

    Update main.py
    removed unnecessary spaces to make it shorter.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ questions_list = [" Q1. What is the name of the youngest sibling of the four?",\
 "Q9. What is the name of the cove in NZ where they teleport to?",\
 "Q10. What evil character returns?"]
 
-
+#Intro
 print("Welcome to my quiz {}, this quiz is about the movie," .format(name))
 print ("The Chronicles of Narnia, Prince Caspian")
 print ("All of your answers will be A, B, C or D")
@@ -25,7 +25,6 @@ print("Lets get started!")
 
 
 #Q1
-
 #this prints the first question on list above.
 print(questions_list[0])
 #possable answers
@@ -34,89 +33,70 @@ print("  A. Peter B. Susan C. Edmund D. Lucy")
 Q1_answer = input("Your answer- \n")
 
 #Answer replys
-
 if Q1_answer == "d":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
-
 #and continues through the code
 
 #Q2
-
-
 print(questions_list[1])
 print("  A.2008  B.2007  C.2009  D.2010 ")
 Q2_answer = input ("Your answer-  \n")
 
 # Answer
-
 if Q2_answer == "a":
     print("Correct 10 points!")
     score += 10
 else:
     print("Sorry that is wrong no points!")
     score += 0
-
 print("Your score is now: {} points \n".format(score))
 
 #Q3
-
 print(questions_list[2])
 print("  A. C.S Lewis  B. Clive Staples Lewis ")
 Q3_answer = input ("Your answer-  \n")
 
 # Answer
-
 if Q3_answer == "A":
     print("Trick question it is both!")
     score += 10
-    
 else:
     print("Trick question it is both!")
     score += 10
-    
 print("Your score is now: {} points".format(score))
 
 #Q4
-
 print(questions_list[3])
 print("  A. True  B. False")
 Q4_answer = input("Your answer- \n")
 
 #Answer
-
 if Q4_answer == "b":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
 
-
 #Q5
-
 print(questions_list[4])
 print("  A. Judaism  B. Islam  C. Christianity")
 Q5_answer = input("Your answer- \n")
 
 #Answer
-
 if Q5_answer == "c":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
-
 
 #Q6
 print(questions_list[5])
@@ -124,52 +104,43 @@ print("  A. Beach  B. Shop  C. Bedroom  D. Train Station")
 Q6_answer = input("Your answer- \n")
 
 #Answer
-
 if Q6_answer == "d":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
 
 #Q7
-
 print(questions_list[6])
 print("  A. True  B. False")
 Q7_answer = input("Your answer- \n")
 
 #Answer
-
 if Q7_answer == "a":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
 
 #Q8
-
 print(questions_list[7])
 print("  A. North  B. South")
 Q8_answer = input("Your answer- \n")
 
 #Answer
-
 if Q8_answer == "a":
  print ("Correct 10 points!")
  score += 10
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
 
 #Q9
-
 print(questions_list[8])
 print("  A. Coverton  B. Akarua  C. Cathedral")
 Q9_answer = input("Your answer- \n")
@@ -182,17 +153,14 @@ if Q9_answer == "c":
 else:
  print("Sorry that is wrong no points!")
  score += 0
- 
 print("Your score is now: {} points\n".format(score))
 
 #Q10
-
 print(questions_list[9])
 print("  A. Caspian  B. Sea Witch  C. Lava Witch D. Ice Witch")
 Q10_answer = input("Your answer- \n")
 
 #Answer
-
 if Q10_answer == "d":
  print ("Correct 10 points!")
  score += 10
@@ -200,9 +168,7 @@ else:
  print("Sorry that is wrong no points!")
  score += 0
  
- 
 #Ending
-  
 print("\n")
 print("Well done!")
 print("Your final score is: {} points\n".format(score))
